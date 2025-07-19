c406e948c29925051cf4f7ad8194f0015ab6c067
NesaraRahal <126090248+NesaraRahal@users.noreply.github.com>
2025-07-19 13:19:31 +0530

    minor changes

--- automax/settings.py
+++ automax/settings.py
@@ -12,8 +12,14 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 from django.contrib.messages import constants as messages
+
+
+#Initialize environ
+env = environ.Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g*3s4ci&txp50-b&oun)uj()oph5(jkfda70g53%itco=rz%kl'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(env('DEBUG'))
 
 ALLOWED_HOSTS = []
 
